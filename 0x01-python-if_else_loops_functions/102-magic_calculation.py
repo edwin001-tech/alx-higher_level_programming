cf47651e9bed67f6fe8f295b4ffedeb4820dd056
edwin001-tech <edwinshitakwa8@gmail.com>
2022-05-31 22:33:40 +0300

    initial commit

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-# 102-magic_calculation.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def magic_calculation(a, b, c):
     """Match bytecode provided by Holberton School."""
     if a < b:
